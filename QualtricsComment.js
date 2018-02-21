c7334cbb58b11bca3fdd1a050937a8d9b9dac5a5
Eric <ericnatsmith@gmail.com>
2018-02-21 11:59:21 -0600

    Now working with image

--- QualtricsComment.js
+++ QualtricsComment.js
@@ -1,16 +1,16 @@
 // Code borrowed from the other sources, and combined by Eric N. Smith
 
 function showComment() /* Show the comment box when comment button is pressed */
-		{
-			var x = document.getElementById("responsebox"); 
-			x.style.display = 'block';
-		}
+    {
+      var x = document.getElementById("responsebox"); 
+      x.style.display = 'block';
+    }
 
 function hideComment() /* Hide the comment box when comment is done or closed*/
-		{
-			var x = document.getElementById("responsebox");
-			x.style.display = 'none';
-		}
+    {
+      var x = document.getElementById("responsebox");
+      x.style.display = 'none';
+    }
 
 $("NextButton").style.cssText = "display:inline;";
 
@@ -56,37 +56,39 @@ function saveCommentData() {
 
 Qualtrics.SurveyEngine.addOnload(function()
 {
-	// This function actually saves the data to qualtrics
-	/*Place your JavaScript here to run when the page loads*/
+  // This function actually saves the data to qualtrics
+  /*Place your JavaScript here to run when the page loads*/
     // https://stackoverflow.com/questions/22851340/in-qualtrics-surveys-how-can-you-save-the-response-to-an-item-into-embedded-dat
     // everything here runs when the page is loaded. So, we can get the Question ID here,
     // but we can't get the response value until later.
-	// testingData = this.getQuestionInfo();
+  // testingData = this.getQuestionInfo();
     currentQuestionID = this.getQuestionInfo().QuestionID
     //var resultEmbeddedName = currentQuestionID + "_result"  //e.g. QID6_result 
     resultEmbeddedName = "comment_" + currentQuestionID   //e.g. result_6
-	//resultEmbeddedName = "comment_" + 'Q3'   //e.g. result_6
-	});
+  //resultEmbeddedName = "comment_" + 'Q3'   //e.g. result_6
+  });
 
 Qualtrics.SurveyEngine.addOnReady(function()
 {
-	/*Place your JavaScript here to run when the page is fully displayed*/
-	 //document.getElementById('moveMe').appendChild(
+  /*Place your JavaScript here to run when the page is fully displayed*/
+   //document.getElementById('moveMe').appendChild(
     //document.getElementById('Buttons')
  // );
-	//$("NextButton").style.cssText = "display:inline;";
-	$("NextButton").setAttribute("style", "display:inline;");
-	$('Buttons').insert($('moveMe').descendants()[0]);
-	jQuery('#QID476 .QuestionBody').appendTo(jQuery("#mychat"));
+  //$("NextButton").style.cssText = "display:inline;";
+  $("NextButton").setAttribute("style", "display:inline;");
+  $('Buttons').insert($('moveMe').descendants()[0]);
+  jQuery('#QID476 .QuestionBody').appendTo(jQuery("#mychat"));
+  jQuery('#SubmitButtonES').appendTo(jQuery("#mychat"));
+  jQuery('#QID476 .TextEntryBox').attr("placeholder", "Tell us more...");
 
 
 
-	
+  
 
 });
 
 Qualtrics.SurveyEngine.addOnUnload(function()
 {
-	/*Place your JavaScript here to run when the page is unloaded*/
+  /*Place your JavaScript here to run when the page is unloaded*/
 
 });
